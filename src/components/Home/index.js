cda46d451f8daedf7c5bc4cf096c483fa0b2ab3d
blackjmxx <alandou.jeanmichel@gmail.com>
2021-03-21 14:47:10 +0100

    upage home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,31 +1,81 @@
 import React from "react";
 import { Grid, Cell } from "styled-css-grid";
-import image1 from '../images/hacking.png';
-import image2 from '../images/cha-ching.png';
+import antiminer from "../images/antiminer.png";
+import hacking from "../images/hacking.png";
+import network from "../images/network.png";
+import chaching from "../images/chaching.png";
+import eth from "../images/eth.png";
+import cpu from "../images/cpu.png";
 
 export const Home = () => (
-    <Grid style={{marginTop:'3%'}} justifyContent={'center'} columns={"1000px"}>
+  <Grid
+    style={{ marginTop: "3%" }}
+    justifyContent={"center"}
+    columns={"1000px"}
+  >
     <Grid columns={"repeat(auto-fit,minmax(300px,1fr))"}>
       <Cell>
-        <img width={200} src={image1} alt="Logo" />
+        <Grid columns={1}>
+          <Cell>
+            <img width={200} src={chaching} alt="Logo" />
+          </Cell>
+          <Cell center>
+            <span>N/A</span>
+          </Cell>
+        </Grid>
       </Cell>
       <Cell>
-        <img width={200} src={image2} alt="Logo" />
+        <Grid columns={1}>
+          <Cell>
+            <img width={200} src={hacking} alt="Logo" />
+          </Cell>
+          <Cell center>
+            <span>N/A</span>
+          </Cell>
+        </Grid>
       </Cell>
       <Cell>
-        <img width={200} src={image1} alt="Logo" />
+        <Grid columns={1}>
+          <Cell>
+            <img width={200} src={network} alt="Logo" />
+          </Cell>
+          <Cell center>
+            <span>N/A</span>
+          </Cell>
+        </Grid>
       </Cell>
     </Grid>
     <Grid columns={"repeat(auto-fit,minmax(300px,1fr))"}>
       <Cell>
-        <img width={200} src={image1} alt="Logo" />
+        <Grid columns={1}>
+          <Cell>
+            <img width={200} src={antiminer} alt="Logo" />
+          </Cell>
+          <Cell center>
+            N/A
+          </Cell>
+        </Grid>
       </Cell>
       <Cell>
-        <img width={200} src={image2} alt="Logo" />
+        <Grid columns={1}>
+          <Cell>
+            <img width={200} src={eth} alt="Logo" />
+          </Cell>
+          <Cell center>
+            <span>N/A</span>
+          </Cell>
+        </Grid>
       </Cell>
       <Cell>
-        <img width={200} src={image1} alt="Logo" />
+        <Grid columns={1}>
+          <Cell>
+            <img width={200} src={cpu} alt="Logo" />
+          </Cell>
+          <Cell center>
+            <span>N/A</span>
+          </Cell>
+        </Grid>
       </Cell>
     </Grid>
   </Grid>
-)
+);
