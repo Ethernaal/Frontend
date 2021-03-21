5327901a5d935cdd5815baf2b7391580085b2281
blackjmxx <alandou.jeanmichel@gmail.com>
2021-03-21 15:01:47 +0100

    black and white pic

--- src/components/Artist/Artists.js
+++ src/components/Artist/Artists.js
@@ -4,6 +4,7 @@ import {Link} from "react-router-dom"
 import artis1 from "../images/artis1.png";
 import artis2 from "../images/artist2.png";
 import artis3 from "../images/artist3.png";
+import tba from "../images/tba.png"
 
 function Artists(props) {
   return (
@@ -18,7 +19,7 @@ function Artists(props) {
             <img width={300} height={300} src={artis1} alt="Logo" />
           </Cell>
           <Cell style={{marginTop:'3%'}} center>
-            <Link to='/artist:1'>TONI BRUTAL - LONDON</Link>
+            <Link to='/artist:1' style={{ color: '#000' }}>TONI BRUTAL - LONDON</Link>
           </Cell>
         </Grid>
         <Grid columns={1}>
@@ -26,7 +27,7 @@ function Artists(props) {
             <img width={300} height={300} src={artis2} alt="Logo" />
           </Cell>
           <Cell style={{marginTop:'3%'}} center>
-            <Link to='/artist:2'>TOM RIBOT - LONDON</Link>
+            <Link to='/artist:2' style={{ color: '#000' }}>TOM RIBOT - LONDON</Link>
           </Cell>
         </Grid>
         <Grid columns={1}>
@@ -34,7 +35,33 @@ function Artists(props) {
             <img width={300} height={300} src={artis3} alt="Logo" />
           </Cell>
           <Cell style={{marginTop:'3%'}} center>
-            <Link to='/artist:3'>TBELEN VELASCO - LONDON</Link>
+            <Link to='/artist:3' style={{ color: '#000' }}>TBELEN VELASCO - LONDON</Link>
+          </Cell>
+        </Grid>
+      </Grid>
+      <Grid columns={"repeat(auto-fit,minmax(300px,1fr))"}>
+        <Grid columns={1}>
+          <Cell>
+            <img width={300} height={300} src={tba} alt="Logo" />
+          </Cell>
+          <Cell style={{marginTop:'3%'}} center>
+            <Link to='/#1' style={{ color: '#000' }}>TBA</Link>
+          </Cell>
+        </Grid>
+        <Grid columns={1}>
+          <Cell>
+            <img width={300} height={300} src={tba} alt="Logo" />
+          </Cell>
+          <Cell style={{marginTop:'3%'}} center>
+            <Link to='/#' style={{ color: '#000' }}>TBA</Link>
+          </Cell>
+        </Grid>
+        <Grid columns={1}>
+          <Cell>
+            <img width={300} height={300} src={tba} alt="Logo" />
+          </Cell>
+          <Cell style={{marginTop:'3%'}} center>
+            <Link to='/#' style={{ color: '#000' }}>TBA</Link>
           </Cell>
         </Grid>
       </Grid>
